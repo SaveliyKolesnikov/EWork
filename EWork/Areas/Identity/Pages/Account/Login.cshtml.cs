d0ee753ba7da87431b391f7194fca4c21dd67b28
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-16 23:05:45 +0300

    Edit loggin page

--- EWork/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ EWork/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -38,8 +38,9 @@ namespace EWork.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            [StringLength(24, MinimumLength = 2, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -74,7 +75,7 @@ namespace EWork.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
--- EWork/Controllers/HomeController.cs
+++ EWork/Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EWork.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EWork.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
