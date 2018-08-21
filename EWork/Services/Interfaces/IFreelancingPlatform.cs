d79eed6fdd1645c1e1ccbe69c339be9b45ff623c
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-21 20:05:37 +0300

    Update a freelancing platform interface

--- EWork/Services/Interfaces/IFreelancingPlatform.cs
+++ EWork/Services/Interfaces/IFreelancingPlatform.cs
@@ -1,25 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using EWork.Models;
-
-namespace EWork.Services.Interfaces
+﻿namespace EWork.Services.Interfaces
 {
     public interface IFreelancingPlatform
     {
-        #region Job
-        Task AddJobAsync(Job job);
-        Task DeleteJobAsync(Job job);
-        Task<Job> FindJobAsync(Predicate<Job> predicate);
-        IQueryable<Job> Jobs { get; }
-        #endregion
-
-        #region Proposal
-        Task AddProposalAsync(Proposal proposal);
-        Task DeleteProposalAsync(Proposal proposal);
-        Task UpdateProposal(Proposal proposal);
-        Task<Proposal> FindProposalAsync(Predicate<Proposal> predicate);
-        IQueryable<Proposal> Proposals { get; }
-        #endregion
+        ITagManager TagManager { get; }
+        IJobManager JobManager { get; }
+        IProposalManager ProposalManager { get; }
     }
 }
