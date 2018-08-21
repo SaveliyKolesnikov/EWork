b69114ec6cddf544bf4ff93fe9da147caca0a033
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-21 19:58:45 +0300

    Add extension methods for more comfortable extracting data from dbsets

---
+++ EWork/Data/Extensions/DataExtractExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EWork.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EWork.Data.Extensions
+{
+    public static class DataExtractExtensions
+    {
+        public static IQueryable<Job> ExtractAll(this DbSet<Job> dbSet)
+        {
+            return dbSet
+                .Include(j => j.Employer)
+                    .ThenInclude(e => e.References)
+                .Include(j => j.JobTags)
+                    .ThenInclude(jt => jt.Tag);
+        }
+
+        public static IQueryable<Proposal> ExtractAll(this DbSet<Proposal> dbSet)
+        {
+            return dbSet
+                .Include(p => p.Sender)
+                    .ThenInclude(f => f.References)
+                        .ThenInclude(r => r.Sender)
+                .Include(p => p.Job)
+                    .ThenInclude(j => j.JobTags)
+                        .ThenInclude(jt => jt.Tag);
+        }
+    }
+}
