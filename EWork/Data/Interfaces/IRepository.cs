f096ea3b50a2fb4cc5c47d66c8d69c1d58086511
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-21 19:58:57 +0300

    Add repository interface

---
+++ EWork/Data/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EWork.Data.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task AddAsync(T item);
+        Task DeleteAsync(T item);
+        Task UpdateAsync(T item);
+        Task<T> FindAsync(Predicate<T> predicate);
+        IQueryable<T> GetAll();
+    }
+}
