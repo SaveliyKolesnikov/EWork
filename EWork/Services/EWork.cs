d6430138c3fe615695e9db3f48d08342ac169896
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-21 20:05:51 +0300

    Update ework

--- EWork/Services/EWork.cs
+++ EWork/Services/EWork.cs
@@ -4,70 +4,26 @@ using System.Linq;
 using System.Security.Authentication;
 using System.Threading.Tasks;
 using EWork.Data;
+using EWork.Data.Extensions;
 using EWork.Models;
 using EWork.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace EWork.Services
 {
     public class EWork : IFreelancingPlatform
     {
-        private readonly IFreelancingPlatiformDbContext _db;
-        private readonly UserManager<User> _userManager;
+        public ITagManager TagManager { get; }
+        public IJobManager JobManager { get; }
+        public IProposalManager ProposalManager { get; }
 
-        public EWork(IFreelancingPlatiformDbContext db, UserManager<User> userManager)
+        public EWork(IJobManager jobManager, IProposalManager proposalManager, ITagManager tagManager)
         {
-            _db = db;
-            _userManager = userManager;
+            JobManager = jobManager;
+            ProposalManager = proposalManager;
+            TagManager = tagManager;
         }
-
-        public async Task AddJobAsync(Job job)
-        {
-            if (!await _userManager.IsInRoleAsync(job.Employer, job.Employer.Role))
-                throw new AuthenticationException($"User must be {job.Employer.Role} for doing this action.");
-
-            await _db.Jobs.AddAsync(job);
-            await _db.SaveChangesAsync();
-        }
-
-        public async Task DeleteJobAsync(Job job)
-        {
-            _db.Jobs.Remove(job);
-            await _db.SaveChangesAsync();
-        }
-
-        public async Task<Job> FindJobAsync(Predicate<Job> predicate) => 
-            await _db.Jobs.FirstOrDefaultAsync(job => predicate(job));
-
-
-        public IQueryable<Job> Jobs => _db.Jobs.AsQueryable();
-    
-
-        public async Task AddProposalAsync(Proposal proposal)
-        {
-            if (!await _userManager.IsInRoleAsync(proposal.Sender, proposal.Sender.Role))
-                throw new AuthenticationException($"User must be {proposal.Sender.Role} for doing this action.");
-
-            await _db.Proposals.AddAsync(proposal);
-            await _db.SaveChangesAsync();
-        }
-
-        public async Task DeleteProposalAsync(Proposal proposal)
-        {
-            _db.Proposals.Remove(proposal);
-            await _db.SaveChangesAsync();
-        }
-
-        public async Task UpdateProposal(Proposal proposal)
-        {
-            _db.Entry(proposal).State = EntityState.Modified;
-            await _db.SaveChangesAsync();
-        }
-
-        public async Task<Proposal> FindProposalAsync(Predicate<Proposal> predicate) =>
-            await _db.Proposals.FirstOrDefaultAsync(proposal => predicate(proposal));
-
-        public IQueryable<Proposal> Proposals => _db.Proposals.AsQueryable();
     }
 }
