1b789916def0195eca846577529fdcdf23b9f73d
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-23 22:58:40 +0300

    Add a view for notifications

---
+++ EWork/wwwroot/js/deleteNotification.js
@@ -0,0 +1,18 @@
+﻿let token = $('input[name="__RequestVerificationToken"]', $('#antiForegeryToken')).val();
+function deleteNotificationPost() {
+    let data = {
+        'notificationId': $(this).data('notificationid'),
+        '__RequestVerificationToken': token
+    };
+    let elem = this;
+    $.post("/Notification/DeleteNotification",
+            data,
+            function () {
+                $(elem).parent().parent().fadeOut();
+            })
+        .fail(function () {
+            alert("Can't delete a notification. Server error.");
+        });
+}
+
+$(".delete-notification").click(deleteNotificationPost);
