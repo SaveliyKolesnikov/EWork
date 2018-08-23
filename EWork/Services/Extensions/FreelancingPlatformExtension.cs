956cae64120f384909eb92e72d3651816d0d3203
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-23 22:47:47 +0300

    Dependency injection update

--- EWork/Services/Extensions/FreelancingPlatformExtension.cs
+++ EWork/Services/Extensions/FreelancingPlatformExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using EWork.Data;
+using EWork.Data.Interfaces;
 using EWork.Data.Repositories;
 using EWork.Models;
 using EWork.Services.Interfaces;
@@ -10,87 +11,38 @@ namespace EWork.Services.Extensions
 {
     public static class FreelancingPlatformExtension
     {
-        public static IServiceCollection AddEWork(this IServiceCollection service)
-        {
-            var serviceProvider = service.BuildServiceProvider();
+        public static IServiceCollection AddEWork(this IServiceCollection service) =>
+            service.AddFreelancingPlatformDbContext().AddRepositories().AddModelManagers()
+                .AddScoped<IFreelancingPlatform, EWork>();
 
-            var db = GetAppDbContext(serviceProvider);
-            var userManager = GetUserManager(serviceProvider);
+        private static IServiceCollection AddModelManagers(this IServiceCollection service) =>
+            service.AddJobManager().AddProposalManager().AddTagManager().AddNotificationManager();
 
-            serviceProvider = service.AddModelManagers(db, userManager).BuildServiceProvider();
+        private static IServiceCollection AddJobManager(this IServiceCollection service) =>
+            service.AddScoped<IJobManager, JobManager>();
 
-            var jobManager = serviceProvider.GetService<IJobManager>();
-            var proposalManager = serviceProvider.GetService<IProposalManager>();
-            var tagManager = serviceProvider.GetService<ITagManager>();
-            var notificationManager = serviceProvider.GetService<INotificationManager>();
+        private static IServiceCollection AddTagManager(this IServiceCollection service) =>
+            service.AddScoped<ITagManager, TagManager>();
 
-            return service.AddScoped<IFreelancingPlatform>(provider => new EWork(jobManager, proposalManager, tagManager, notificationManager));
-        }
+        private static IServiceCollection AddProposalManager(this IServiceCollection service) =>
+            service.AddScoped<IProposalManager, ProposalManager>();
 
-        private static IServiceCollection AddModelManagers(this IServiceCollection service, ApplicationDbContext db = null, UserManager<User> userManager = null)
-        {
-            if (db is null)
-                db = GetAppDbContext(service);
+        private static IServiceCollection AddNotificationManager(this IServiceCollection service) =>
+            service.AddScoped<INotificationManager, NotificationManager>();
 
-            if (userManager is null)
-                userManager = GetUserManager(service);
+        private static IServiceCollection AddRepositories(this IServiceCollection service) =>
+            service.AddProposalRepository().AddNotificationRepository().AddJobRepository();
 
-            return service.AddJobManager(db, userManager)
-                .AddProposalManager(db, userManager)
-                .AddTagManager(db)
-                .AddNotificationManager(userManager);
-        }
+        private static IServiceCollection AddProposalRepository(this IServiceCollection service) =>
+            service.AddScoped<IRepository<Proposal>, ProposalRepository>();
 
-        private static IServiceCollection AddJobManager(this IServiceCollection service, ApplicationDbContext db = null, UserManager<User> userManager = null)
-        {
-            if (db is null)
-                db = GetAppDbContext(service);
+        private static IServiceCollection AddNotificationRepository(this IServiceCollection service) =>
+            service.AddScoped<IRepository<Notification>, NotificationRepository>();
 
-            if (userManager is null)
-                userManager = GetUserManager(service);
+        private static IServiceCollection AddJobRepository(this IServiceCollection service) =>
+            service.AddScoped<IRepository<Job>, JobRepository>();
 
-            var jobRepository = new JobRepository(db, userManager);
-            return service.AddScoped<IJobManager>(provider => new JobManager(jobRepository));
-        }
-
-        private static IServiceCollection AddTagManager(this IServiceCollection service, ApplicationDbContext db = null)
-        {
-            if (db is null)
-                db = GetAppDbContext(service);
-
-            return service.AddScoped<ITagManager>(provider => new TagManager(db));
-        }
-
-        private static IServiceCollection AddProposalManager(this IServiceCollection service, ApplicationDbContext db = null, UserManager<User> userManager = null)
-        {
-            if (db is null)
-                db = GetAppDbContext(service);
-
-            if (userManager is null)
-                userManager = GetUserManager(service);
-
-            var proposalRepository = new ProposalRepository(db, userManager);
-            return service.AddScoped<IProposalManager>(provider => new ProposalManager(proposalRepository));
-        }
-
-        private static IServiceCollection AddNotificationManager(this IServiceCollection service, UserManager<User> userManager = null)
-        {
-            if (userManager is null)
-                userManager = GetUserManager(service);
-
-            return service.AddScoped<INotificationManager>(provider => new NotificationManager(userManager));
-        }
-
-        private static ApplicationDbContext GetAppDbContext(IServiceCollection service) =>
-            GetAppDbContext(service.BuildServiceProvider());
-
-        private static UserManager<User> GetUserManager(IServiceCollection service) =>
-            GetUserManager(service.BuildServiceProvider());
-
-        private static ApplicationDbContext GetAppDbContext(IServiceProvider serviceProvider) =>
-            serviceProvider.GetService<ApplicationDbContext>();
-
-        private static UserManager<User> GetUserManager(IServiceProvider serviceProvider) =>
-            serviceProvider.GetService<UserManager<User>>();
+        private static IServiceCollection AddFreelancingPlatformDbContext(this IServiceCollection service) =>
+            service.AddScoped<IFreelancingPlatiformDbContext, ApplicationDbContext>();
     }
 }
