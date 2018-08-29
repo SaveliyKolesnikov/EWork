373da3e2be213eb16915e46551c1b2a9ee39c1d9
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-29 18:33:35 +0300

    Update user photo display

---
+++ EWork/Migrations/20180829151423_ReanmeProfilePhotoProperty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EWork.Migrations
+{
+    public partial class ReanmeProfilePhotoProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfilePhotoUrl",
+                table: "AspNetUsers",
+                newName: "ProfilePhotoName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfilePhotoName",
+                table: "AspNetUsers",
+                newName: "ProfilePhotoUrl");
+        }
+    }
+}
