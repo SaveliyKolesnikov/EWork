cb3a7128c91e42034b7882c21d553dd727d46194
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-29 18:09:26 +0300

    Add max size property

--- EWork/Config/PhotoConfig.cs
+++ EWork/Config/PhotoConfig.cs
@@ -4,6 +4,7 @@ namespace EWork.Config
 {
     public class PhotoConfig
     {
+        public long MaxSize { get; set; }
         public string DefaultPhoto { get; set; }
         public string UsersPhotosPath { get; set; }
         public IEnumerable<string> AllowedExtensions { get; set; }
