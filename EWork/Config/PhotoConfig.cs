7c74578de53a78e2b14244e4aa79c428c195e922
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-29 16:09:12 +0300

    Add profile config model

---
+++ EWork/Config/PhotoConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace EWork.Config
+{
+    public class PhotoConfig
+    {
+        public string DefaultPhoto { get; set; }
+        public string UsersPhotosPath { get; set; }
+        public IEnumerable<string> AllowedExtensions { get; set; }
+    }
+}
