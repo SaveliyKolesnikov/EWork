458fc75b2413e30d9a288e7173bd917ceec0a11a
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-30 12:58:25 +0300

    Minor improvements

--- EWork/Data/Repositories/JobRepository.cs
+++ EWork/Data/Repositories/JobRepository.cs
@@ -68,7 +68,7 @@ namespace EWork.Data.Repositories
         }
 
         public async Task<Job> FindAsync(Predicate<Job> predicate) =>
-            await _db.Jobs.ExtractAll().FirstOrDefaultAsync(job => predicate(job));
+            await GetAll().FirstOrDefaultAsync(job => predicate(job));
 
         public IQueryable<Job> GetAll() => _db.Jobs.ExtractAll();
     }
