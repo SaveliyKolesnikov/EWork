ffc9c37269ebca8bc960f1f7e1989f0c44395e9d
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-02 12:45:56 +0300

    Add the chat view model

---
+++ EWork/ViewModels/ChatViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EWork.Models;
+
+namespace EWork.ViewModels
+{
+    public class ChatViewModel
+    {
+        public ChatViewModel(Message message, User reciever = null)
+        {
+            Message = message;
+            Reciever = reciever;
+        }
+
+        public Message Message { get; }
+        public User Reciever { get; }
+    }
+}
