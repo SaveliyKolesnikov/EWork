d0351b638e572e58fa78aa6178c626cc9bed85c5
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-02 12:07:43 +0300

    Add the message manager

---
+++ EWork/Services/MessageManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EWork.Data.Interfaces;
+using EWork.Models;
+using EWork.Services.Interfaces;
+
+namespace EWork.Services
+{
+    public class MessageManager : IMessageManager
+    {
+        private readonly IRepository<Message> _repository;
+
+        public MessageManager(IRepository<Message> repository) => _repository = repository;
+
+        public Task AddAsync(Message item) => _repository.AddAsync(item);
+
+        public Task DeleteAsync(Message item) => _repository.DeleteAsync(item);
+
+        public Task DeleteRangeAsync(IEnumerable<Message> items) => _repository.DeleteRangeAsync(items);
+
+        public Task<Message> FindAsync(Predicate<Message> predicate) => _repository.FindAsync(predicate);
+
+        public IQueryable<Message> GetAll() => _repository.GetAll();
+
+        public Task UpdateAsync(Message item) => _repository.UpdateAsync(item);
+    }
+}
