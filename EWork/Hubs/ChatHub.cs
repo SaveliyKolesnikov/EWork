751dd4972a284b3c1d5851c37713e931395153f4
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-02 16:58:22 +0300

    Add the chat hub

---
+++ EWork/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatAuth.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAuth.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message) =>
+            await Clients.All.SendAsync("receiveMessage", message);
+    }
+}
