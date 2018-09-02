6f057119fa9d2df8f50845da16ae1cb5215176aa
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-02 11:44:42 +0300

    Add the chat controller

---
+++ EWork/Controllers/ChatController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EWork.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
