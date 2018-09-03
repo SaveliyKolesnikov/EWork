d5efd966c6fdaf5d18f8919ef91f085f14fbdfe8
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-04 01:10:08 +0300

    Add an ajax messages downloading

--- EWork/wwwroot/js/chat/chat.js
+++ EWork/wwwroot/js/chat/chat.js
@@ -5,6 +5,10 @@ const receiverInput = document.getElementById('receiverUserName');
 const chat = document.getElementById('chat-history');
 const messagesQueue = [];
 
+if (makeDialogActiveQuery !== undefined) {
+    makeDialogActive($(makeDialogActiveQuery));
+}
+
 document.getElementById('submitMessageButton').addEventListener('click', function (e) {
     e.preventDefault();
     sendDateInput.value = new Date().format("m/dd/yyyy hh:MM TT");
@@ -13,7 +17,7 @@ document.getElementById('submitMessageButton').addEventListener('click', functio
 });
 
 function clearInputField() {
-    let newMessage = new Message(senderUserName.value, receiverInput.value, textInput.value, new Date());
+    let newMessage = new Message(new User(currentUserName, ""), new User(receiverInput.value, ""), textInput.value, new Date());
     messagesQueue.push(newMessage);
     textInput.value = "";
 }
@@ -30,9 +34,27 @@ function addMessageToChat(message) {
     //    if (currentReceiver !== message.receiverUserName && currentReceiver !== message.senderUserName) 
     //      return;
 
-    let senderName = message.receiverUserName === currentUserName ? message.senderUserName : message.receiverUserName;
+    let senderName = message.receiver.userName === currentUserName ? message.sender.userName : message.receiver.userName;
     if (receiverInput.value !== senderName) {
         let recMesBar = $(`.message-bar-elem[data-receiverusername='${senderName}']`);
+        if (recMesBar.length === 0) {
+            let newMessageBarElem = document.createElement('div');
+            newMessageBarElem.innerHTML = `<div class="message-bar-elem row" data-receiverusername="${senderName}">
+                                               <div class="row">
+                                                   <div class="col-xs-12 col-md-4 text-center">
+                                                       <img class="profile-photo" src="${message.sender.photoUrl}" alt="Profile photo" />
+                                                   </div>
+                                                   <div class="col-md-8 visible-md visible-lg visible-sm">
+                                                       <div class="receiver-name">${message.sender.userName}</div>
+                                                       <div class="message-preview">${message.text}</div>
+                                                   </div>
+                                               </div>
+                                           </div>`;
+
+            document.getElementById('message-bars-container').appendChild(newMessageBarElem);
+
+            recMesBar = $(`.message-bar-elem[data-receiverusername='${senderName}']`);
+        }
         $(recMesBar)
             .css('order', `${maxOrder++}`)
             .addClass('new-message');
@@ -40,7 +62,7 @@ function addMessageToChat(message) {
         return;
     }
 
-    let isCurrentUserMessage = message.senderUserName === currentUserName;
+    let isCurrentUserMessage = message.sender.userName === currentUserName;
 
     let container = document.createElement('div');
     container.className = isCurrentUserMessage ? "container darker" : "container";
@@ -55,4 +77,40 @@ function addMessageToChat(message) {
     container.appendChild(text);
     container.appendChild(when);
     chat.appendChild(container);
+    chat.scrollTop = chat.scrollHeight;
+}
+
+$('.message-bar-elem').click(function () {
+    chat.innerHTML = '<div class="text-center">Downloading messages</div>';
+    makeDialogActive(this);
+    let newReceiverUsername = receiverInput.value;
+    let form = new FormData();
+    let token = $('input[name="__RequestVerificationToken"]', $('#AntiForgeryToken')).val();
+    form.append('__RequestVerificationToken', token);
+    form.append('username1', newReceiverUsername);
+    form.append('username2', currentUserName);
+
+    fetch('/Chat/GetMessages', {
+        method: "POST",
+        credentials: 'include',
+        body: form
+    })
+        .then(res => {
+            var messages = res.json().then(messages => {
+                $(chat).empty();
+                for (let mes of messages) {
+                    addMessageToChat(mes);
+                }
+            });
+        });
+});
+
+function makeDialogActive(dialogElem) {
+    if ($(dialogElem).hasClass('active'))
+        return;
+
+    $('.message-bar-elem.active').removeClass('active');
+    $(dialogElem).addClass('active');
+
+    receiverInput.value = $(dialogElem).data('receiverusername');
 }
