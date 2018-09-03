20f674ae3cf87220413ff68e9c1f4ec9d6cc423f
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-03 16:01:07 +0300

    Add a chat js logic

---
+++ EWork/wwwroot/js/chat/chat.js
@@ -0,0 +1,53 @@
+﻿// userName is declared in razor page.
+const textInput = document.getElementById('messageText');
+const sendDateInput = document.getElementById('sendDate');
+const receiverInput = document.getElementById('receiverUserName');
+const chat = document.getElementById('chat-history');
+const messagesQueue = [];
+
+document.getElementById('submitMessageButton').addEventListener('click', function (e) {
+    e.preventDefault();
+    sendDateInput.value = new Date().format("m/dd/yyyy hh:MM TT");
+    clearInputField();
+    sendMessage();
+});
+
+function clearInputField() {
+    let newMessage = new Message(senderUserName.value, receiverInput.value, textInput.value, new Date());
+    messagesQueue.push(newMessage);
+    textInput.value = "";
+}
+
+function sendMessage() {
+    let message = messagesQueue.shift() || "";
+    if (message === "" || message.text.trim() === "")
+        return;
+
+    sendMessageToHub(message);
+}
+
+function addMessageToChat(message) {
+    if (currentReceiver !== message.receiverUserName && currentReceiver !== message.senderUserName) 
+        return;
+
+    let senderName = message.receiverUserName === currentReceiver ? message.senderUserName : message.receiverUserName;
+    if (receiverInput.value !== senderName) {
+        $(".message-bar-elem").find(`[data-receiverusername='${senderName}']`)
+    }
+
+    let isCurrentUserMessage = message.senderUserName === currentUserName;
+
+    let container = document.createElement('div');
+    container.className = isCurrentUserMessage ? "container darker" : "container";
+
+    let text = document.createElement('p');
+    text.innerHTML = message.text;
+
+    let when = document.createElement('span');
+    when.className = isCurrentUserMessage ? "time-left" : "time-right";
+    when.innerHTML = new Date(message.sendDate).format("m/dd/yyyy hh:MM TT");
+
+    container.appendChild(text);
+    container.appendChild(when);
+    chat.appendChild(container);
+}
