7b0de9886b89aa29d4ce1677e9bf202e312867d9
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-03 15:53:37 +0300

    Add a currentuser property to chat view model

--- EWork/ViewModels/ChatViewModel.cs
+++ EWork/ViewModels/ChatViewModel.cs
@@ -8,14 +8,16 @@ namespace EWork.ViewModels
 {
     public class ChatViewModel
     {
-        public ChatViewModel(IQueryable<Message> messages, string pathToProfilePhotos, User receiver = null)
+        public ChatViewModel(User currentUser, IQueryable<Message> messages, string pathToProfilePhotos, User receiver = null)
         {
             PathToProfilePhotos = pathToProfilePhotos;
+            CurrentUser = currentUser;
             Messages = messages;
             Receiver = receiver;
         }
 
         public IQueryable<Message> Messages { get; }
+        public User CurrentUser { get; }
         public User Receiver { get; }
         public string PathToProfilePhotos { get; }
     }
