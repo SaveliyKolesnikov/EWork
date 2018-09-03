7a9a3e1dcf8a11b3166e75efd2e1d6b218f52f94
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-03 23:45:08 +0300

    Add a JsonUser model

---
+++ EWork/Models/Json/JsonUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EWork.Models.Json
+{
+    public class JsonUser
+    {
+        public JsonUser(string userName, string photoUrl)
+        {
+            UserName = userName;
+            PhotoUrl = photoUrl;
+        }
+
+        public string UserName { get; }
+        public string PhotoUrl { get; }
+    }
+}
