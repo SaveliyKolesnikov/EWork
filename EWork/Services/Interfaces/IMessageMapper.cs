3d948aad5fc34041d47f7bdcd4d2aded4079a744
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-03 23:46:18 +0300

    Add IMessageMapper

---
+++ EWork/Services/Interfaces/IMessageMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EWork.Models;
+using EWork.Models.Json;
+
+namespace EWork.Services.Interfaces
+{
+    public interface IMessageMapper
+    {
+        JsonMessage Map(Message message);
+        IEnumerable<JsonMessage> MapRange(IEnumerable<Message> message);
+    }
+}
