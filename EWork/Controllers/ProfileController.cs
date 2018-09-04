9b0a20ea4ee4992cf6e80e15387e908b8355774d
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-05 00:19:15 +0300

    Update a value passed into profile view

--- EWork/Controllers/ProfileController.cs
+++ EWork/Controllers/ProfileController.cs
@@ -7,6 +7,7 @@ using EWork.Config;
 using EWork.Models;
 using EWork.Services.Interfaces;
 using EWork.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,16 +15,19 @@ using Microsoft.Extensions.Options;
 
 namespace EWork.Controllers
 {
+    [Authorize]
     public class ProfileController : Controller
     {
         private readonly UserManager<User> _userManager;
         private readonly IReviewManager _reviewManager;
+        private readonly IJobManager _jobManager;
         private readonly IOptions<PhotoConfig> _photoOptions;
 
-        public ProfileController(UserManager<User> userManager, IReviewManager reviewManager, IOptions<PhotoConfig> photoOptions)
+        public ProfileController(UserManager<User> userManager, IReviewManager reviewManager,IJobManager jobManager, IOptions<PhotoConfig> photoOptions)
         {
             _userManager = userManager;
             _reviewManager = reviewManager;
+            _jobManager = jobManager;
             _photoOptions = photoOptions;
         }
 
@@ -33,8 +37,11 @@ namespace EWork.Controllers
             if (user is null)
                 return BadRequest();
 
+            var currentUser = await _userManager.GetUserAsync(User);
+            var isCurrentUserCanAddReview =
+                await _jobManager.GetAll().AnyAsync(j => j.IsClosed && j.HiredFreelancer.Id == currentUser.Id);
             user.Reviews = await _reviewManager.GetAll().Where(r => r.User.Id == user.Id).ToListAsync();
-            var profileViewModel = new ProfileViewModel(user, _photoOptions.Value.UsersPhotosPath);
+            var profileViewModel = new ProfileViewModel(user, currentUser, _photoOptions.Value.UsersPhotosPath, isCurrentUserCanAddReview);
             return View(profileViewModel);
         }
     }
