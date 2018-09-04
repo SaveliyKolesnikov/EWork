a23204d674fa8465f2a8a8bc687a4344b2b1a09d
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-04 23:03:27 +0300

    Add IsCanAddReviewProperty to a profile view model

--- EWork/ViewModels/ProfileViewModel.cs
+++ EWork/ViewModels/ProfileViewModel.cs
@@ -8,6 +8,10 @@ namespace EWork.ViewModels
 {
     public class ProfileViewModel
     {
+        public ProfileViewModel(User currentUser, string pathToProfilePhotos, bool isCanAddReview = false) :
+            this(currentUser, pathToProfilePhotos) =>
+            IsCanAddReview = isCanAddReview;
+
         public ProfileViewModel(User currentUser, string pathToProfilePhotos)
         {
             CurrentUser = currentUser;
@@ -16,5 +20,6 @@ namespace EWork.ViewModels
 
         public User CurrentUser { get; }
         public string PathToProfilePhotos { get; }
+        public bool IsCanAddReview { get; }
     }
 }
