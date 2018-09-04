fdba865b946d0b6b130a6852bca2c8cbb55ae170
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-05 00:16:52 +0300

    Add a profileOwner property to a profile view model

--- EWork/ViewModels/ProfileViewModel.cs
+++ EWork/ViewModels/ProfileViewModel.cs
@@ -8,18 +8,21 @@ namespace EWork.ViewModels
 {
     public class ProfileViewModel
     {
-        public ProfileViewModel(User currentUser, string pathToProfilePhotos, bool isCanAddReview = false) :
-            this(currentUser, pathToProfilePhotos) =>
+        public ProfileViewModel(User profileOwner, User currentUser, string pathToProfilePhotos, bool isCanAddReview) :
+            this(profileOwner, currentUser, pathToProfilePhotos) =>
             IsCanAddReview = isCanAddReview;
 
-        public ProfileViewModel(User currentUser, string pathToProfilePhotos)
+        public ProfileViewModel(User profileOwner, User currentUser, string pathToProfilePhotos)
         {
-            CurrentUser = currentUser;
+            ProfileOwner = profileOwner ?? throw new ArgumentNullException(nameof(profileOwner));
+            CurrentUser = currentUser ?? throw new ArgumentNullException(nameof(currentUser));
             PathToProfilePhotos = pathToProfilePhotos;
         }
 
+        public User ProfileOwner { get; }
         public User CurrentUser { get; }
         public string PathToProfilePhotos { get; }
         public bool IsCanAddReview { get; }
     }
 }
+
