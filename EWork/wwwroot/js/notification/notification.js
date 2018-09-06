dde651076d5e9dd7c71b5934fda6eb361660e3df
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-07 01:45:30 +0300

    Add request handlers scripts

---
+++ EWork/wwwroot/js/notification/notification.js
@@ -0,0 +1,10 @@
+﻿const numOfNotifications = document.getElementById('numOfNotifications');
+
+function newNotification() {
+    let currentNum = parseInt(numOfNotifications.innerHTML);
+
+    if (currentNum === 0)
+        numOfNotifications.color = "red";
+
+    numOfNotifications.innerHTML = ++currentNum;
+}
