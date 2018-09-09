b0d7a819de7120ed48ce6079ac77601669f67f75
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-09 14:39:42 +0300

    Add a notification view model

---
+++ EWork/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EWork.Models;
+
+namespace EWork.ViewModels
+{
+    public class NotificationViewModel
+    {
+        public NotificationViewModel(User currentUser, IEnumerable<Notification> notifications)
+        {
+            CurrentUser = currentUser;
+            Notifications = notifications;
+        }
+
+        public User CurrentUser { get; }
+        public IEnumerable<Notification> Notifications { get; }
+    }
+}
