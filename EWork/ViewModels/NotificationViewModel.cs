a8e7524f3c212cf7952bff4a272193e3f6e43ca2
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-09 15:56:02 +0300

    Add a take amount property to the notification view model.

--- EWork/ViewModels/NotificationViewModel.cs
+++ EWork/ViewModels/NotificationViewModel.cs
@@ -8,13 +8,15 @@ namespace EWork.ViewModels
 {
     public class NotificationViewModel
     {
-        public NotificationViewModel(User currentUser, IEnumerable<Notification> notifications)
+        public NotificationViewModel(User currentUser, IEnumerable<Notification> notifications, int takeAmount = 5)
         {
             CurrentUser = currentUser;
             Notifications = notifications;
+            TakeAmount = takeAmount;
         }
 
         public User CurrentUser { get; }
         public IEnumerable<Notification> Notifications { get; }
+        public int TakeAmount { get; }
     }
 }
