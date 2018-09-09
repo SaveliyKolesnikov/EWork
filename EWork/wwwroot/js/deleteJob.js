5a5e9d8e9c0d3fc0688977ed5f6fcb856b84db2a
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-09 16:05:06 +0300

    Minor improvements.

--- EWork/wwwroot/js/deleteJob.js
+++ EWork/wwwroot/js/deleteJob.js
@@ -3,6 +3,7 @@
 function deleteJob() {
     let deletedJobId = $(this).data("job-id");
     let deletedElement = $(this).parent().parent();
+    // TODO: Antiforgerytoken
     $.post('DeleteJob',
             { jobId: deletedJobId })
         .done(() => deletedElement.remove());
