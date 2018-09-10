29e88e068d84760684559dd9dcf91066881d7ea3
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-10 12:50:45 +0300

    Add an ajax method url property to the job board view model.

--- EWork/ViewModels/JobBoardViewModel.cs
+++ EWork/ViewModels/JobBoardViewModel.cs
@@ -8,11 +8,12 @@ namespace EWork.ViewModels
 {
     public class JobBoardViewModel
     {
-        public JobBoardViewModel(IQueryable<Job> jobs, IEnumerable<string> usedTags, string searchUrl, int takeAmount = 5)
+        public JobBoardViewModel(IQueryable<Job> jobs, IEnumerable<string> usedTags, string searchUrl, string getJobsAjaxMethodUrl, int takeAmount = 5)
         {
             Jobs = jobs;
             UsedTags = usedTags;
             SearchUrl = searchUrl;
+            GetJobsAjaxMethodUrl = getJobsAjaxMethodUrl;
             TakeAmount = takeAmount;
         }
 
@@ -20,5 +21,6 @@ namespace EWork.ViewModels
         public IEnumerable<string> UsedTags { get; }
         public int TakeAmount { get; }
         public string SearchUrl { get; }
+        public string GetJobsAjaxMethodUrl { get; }
     }
 }
