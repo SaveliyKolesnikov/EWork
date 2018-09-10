bd9e647ffb0f3362420e6f50e1707537e566cb34
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-10 13:26:56 +0300

    Add an ajax job downloading method.

---
+++ EWork/wwwroot/js/job/ajaxJobDownload.js
@@ -0,0 +1,25 @@
+﻿$('#download-more-notifications').click(function() {
+    const takeAmount = 5;
+    downloadJobs(takeAmount);
+});
+
+function downloadJobs(takeAmount) {
+    let token = $('input[name="__RequestVerificationToken"]', $('#antiForgeryToken')).val();
+    let amountOfJobsNow = $('.job-container').length;
+    let requiredTags = $('#SearchingByTags').val();
+    let getJobsAjaxMethod = $('input[name="getJobsAjaxMethodUrl"]').val();
+
+    $.post(getJobsAjaxMethod,
+        {
+            '__RequestVerificationToken': token,
+            'skipAmount': amountOfJobsNow,
+            'takeAmount': takeAmount,
+            'requiredTags': requiredTags
+        },
+        function (jobs) {
+            jobs.forEach(addJob);
+        }).fail(
+        function (errorObj) {
+            console.error(errorObj.responseJSON.message);
+        });
+}
