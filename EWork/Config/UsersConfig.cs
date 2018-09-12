9fa83c3780e894e949d367a9aaedc36fabfc0547
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-12 16:42:56 +0300

    Add users config model

---
+++ EWork/Config/UsersConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+
+namespace EWork.Config
+{
+    public class UsersConfig
+    {
+        public List<UserConfig> Administrators{ get; set; }
+        public List<UserConfig> Moderators { get; set; }
+
+        public class UserConfig
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Email { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
