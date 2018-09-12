e3ba643ebd4d71b850cff4db1183fe4652e0ca48
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-12 19:57:26 +0300

    Add an admin page view model.

---
+++ EWork/ViewModels/AdminPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EWork.ViewModels
+{
+    public class AdminPageViewModel<T> where T : class
+    {
+        public AdminPageViewModel(IEnumerable<T> items, string searchString = "")
+        {
+            Items = items;
+            SearchString = searchString;
+        }
+
+        public IEnumerable<T> Items { get; }
+        public string SearchString { get; }
+    }
+}
