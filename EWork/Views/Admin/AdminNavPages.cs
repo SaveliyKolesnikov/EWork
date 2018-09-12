dae6aebab0f644b244807bf18fb282d844bf212f
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-12 18:44:22 +0300

    Add admin nav pages.

---
+++ EWork/Views/Admin/AdminNavPages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AdminNavPages
+{ 
+    public static class AdminNavPages
+    {
+        public static string Index => "AllJobs";
+
+        public static string Users => "Users";
+        public static string OpenedDisputes => "OpenedDisputes";
+
+        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+
+        public static string UsersNavClass(ViewContext viewContext) => PageNavClass(viewContext, Users);
+        public static string OpenedDisputesNavClass(ViewContext viewContext) => PageNavClass(viewContext, OpenedDisputes);
+
+        public static string PageNavClass(ViewContext viewContext, string page)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string
+                ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+        }
+    }
+}
