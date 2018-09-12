44eb3538e19636f54e0d3ce4563cf8c0698b6de4
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-12 16:16:00 +0300

    Add an administrator model.

---
+++ EWork/Migrations/20180912131528_addAdministratorModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EWork.Migrations
+{
+    public partial class addAdministratorModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdministratorId",
+                table: "Jobs",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_AdministratorId",
+                table: "Jobs",
+                column: "AdministratorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_AspNetUsers_AdministratorId",
+                table: "Jobs",
+                column: "AdministratorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_AspNetUsers_AdministratorId",
+                table: "Jobs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Jobs_AdministratorId",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "AdministratorId",
+                table: "Jobs");
+        }
+    }
+}
