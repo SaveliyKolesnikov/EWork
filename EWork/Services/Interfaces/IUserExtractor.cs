45f989b6ecb08b46249adf18cd933b75c1058976
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-12 19:49:01 +0300

    Add the IUserExtractor interface.

---
+++ EWork/Services/Interfaces/IUserExtractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EWork.Models;
+
+namespace EWork.Services.Interfaces
+{
+    public interface IUserExtractor
+    {
+        IQueryable<User> GetAll();
+
+        Task<User> FindAsync(Predicate<User> predicate);
+    }
+}
