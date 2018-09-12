69dcb1926dbfc2ba3486865b9cfa1dd1d4799e23
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-12 19:49:14 +0300

    Add an user extractor

---
+++ EWork/Services/UserExtractor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EWork.Data.Interfaces;
+using EWork.Models;
+using EWork.Services.Interfaces;
+
+namespace EWork.Services
+{
+    public class UserExtractor : IUserExtractor
+    {
+        private readonly IRepository<User> _repository;
+
+        public UserExtractor(IRepository<User> repository) => _repository = repository;
+
+        public IQueryable<User> GetAll() => _repository.GetAll();
+
+        public Task<User> FindAsync(Predicate<User> predicate) => _repository.FindAsync(predicate);
+    }
+}
