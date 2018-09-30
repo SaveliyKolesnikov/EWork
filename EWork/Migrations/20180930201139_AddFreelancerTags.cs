4e8c1cacb21081fef7976e02265bbafb21e5d7d1
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-09-30 23:15:42 +0300

    Freelancers tags migrations

---
+++ EWork/Migrations/20180930201139_AddFreelancerTags.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EWork.Migrations
+{
+    public partial class AddFreelancerTags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FreelancerTags",
+                columns: table => new
+                {
+                    FreelancerId = table.Column<string>(nullable: false),
+                    TagId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FreelancerTags", x => new { x.FreelancerId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_FreelancerTags_AspNetUsers_FreelancerId",
+                        column: x => x.FreelancerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FreelancerTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FreelancerTags_TagId",
+                table: "FreelancerTags",
+                column: "TagId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FreelancerTags");
+        }
+    }
+}
