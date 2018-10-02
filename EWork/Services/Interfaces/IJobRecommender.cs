669303a073e6e44b21e5c3b00e639c3b49c77fc9
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-10-03 00:09:05 +0300

    Add the JobRecommender service.

---
+++ EWork/Services/Interfaces/IJobRecommender.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using EWork.Models;
+
+namespace EWork.Services.Interfaces
+{
+    public interface IJobRecommender
+    {
+        Task RecommendAsync(Job job);
+    }
+}
