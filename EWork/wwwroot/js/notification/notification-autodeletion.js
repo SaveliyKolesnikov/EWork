2c052f421e9d19f93c4af1958b0e62eeb9462abb
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-11-02 16:01:50 +0200

    Add changing autodeletion status.

---
+++ EWork/wwwroot/js/notification/notification-autodeletion.js
@@ -0,0 +1,17 @@
+﻿$('.autodeletion-button').click(function() {
+    if ($('.autodeletion-checkbox').is(':checked'))
+        DisableNotificationsAutodeletion();
+    else
+        EnableNotificationsAutodeletion();
+});
+
+function DisableNotificationsAutodeletion() {
+    $('.autodeletion-checkbox').attr('checked', false);
+    $('.autodeletion-button').removeClass('blue').html('OFF');
+}
+
+function EnableNotificationsAutodeletion() {
+    $('.autodeletion-checkbox').attr('checked', true);
+    $('.autodeletion-button').addClass('blue').html('ON');
+
+}
