366323ae28465e205768a608524dbd0187e2993d
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-11-02 16:20:02 +0200

    Add autodeletion logic methods

--- EWork/wwwroot/js/notification/notification-autodeletion.js
+++ EWork/wwwroot/js/notification/notification-autodeletion.js
@@ -1,17 +1,25 @@
 ﻿$('.autodeletion-button').click(function() {
     if ($('.autodeletion-checkbox').is(':checked'))
-        DisableNotificationsAutodeletion();
+        disableNotificationsAutodeletion();
     else
-        EnableNotificationsAutodeletion();
+        enableNotificationsAutodeletion();
 });
 
-function DisableNotificationsAutodeletion() {
+function disableNotificationsAutodeletion() {
     $('.autodeletion-checkbox').attr('checked', false);
     $('.autodeletion-button').removeClass('blue').html('OFF');
 }
 
-function EnableNotificationsAutodeletion() {
+function enableNotificationsAutodeletion() {
     $('.autodeletion-checkbox').attr('checked', true);
     $('.autodeletion-button').addClass('blue').html('ON');
-
 }
+
+function onSourceLinkClick(e) {
+    if (!$('.autodeletion-checkbox').is(':checked'))
+        return;
+
+    const deleteNotificationLink = $('.delete-notification', $(e.target).parent().parent());
+    deleteNotificationLink.click();
+}
+
